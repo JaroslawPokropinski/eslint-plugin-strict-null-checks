fafc11659157320fc4762365c8f4c90ab32d8fa5
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2022-03-06 00:18:11 +0100

    remove dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const requireIndex = require("requireindex");
-const obj = requireIndex(__dirname + "/rules");
-const rules = {};
-Object.keys(obj).forEach(
-  (ruleName) => (rules[ruleName] = obj[ruleName].default)
-);
-// import all rules in src/rules
-module.exports = { rules };
--- dist/utils/create-eslint-rule.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createEslintRule = void 0;
-const utils_1 = require("@typescript-eslint/utils");
-exports.createEslintRule = utils_1.ESLintUtils.RuleCreator((ruleName) => ruleName);
