84c33ce492516478a4b07517a395265cf277ed10
Alexander Filimonov <wrager@mail.ru>
2022-11-13 18:44:25 +0300

    Fix valid tests running

--- test/rules/all.test.ts
+++ test/rules/all.test.ts
@@ -11,7 +11,7 @@ const ruleTester: RuleTester = new RuleTester({
     tsconfigRootDir: root,
   },
 });
-const valid = [
+const validStatements = [
   `
   function foo() {
     const a = 1 as number | undefined;
@@ -128,6 +128,10 @@ const invalidFunctionArguments = [
   `,
 ];
 
+const valid = validStatements.map((st) => ({
+  code: st,
+}));
+
 const invalid = [
   ...invalidStatemetsMemberAccess.map((st) => ({
     code: st,
@@ -144,6 +148,6 @@ const invalid = [
 ];
 
 ruleTester.run(RULE_NAME, rule, {
-  valid: [],
+  valid,
   invalid,
 });
